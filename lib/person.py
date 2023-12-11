e757b34840d56dfd50bfaccc53ed7fc056e72185
Joses Syanwa <josesyanwa@gmail.com>
2023-12-11 13:51:39 +0300

    Completed assignment

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
-
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method for talking
+    def talk(self):
+        print("Hello World!")
+
+    # Instance method for walking
+    def walk(self):
+        print("The person is walking.")
